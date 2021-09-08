fe6e0b2187f8606c11d51144545d834766635f9b
Lacey <lacey@avinc.com>
2021-09-08 15:45:23 -0700

    Updated analyze_vio_trajectories.py

--- scripts/analyze_vio_trajectories.py
+++ scripts/analyze_vio_trajectories.py
@@ -183,3 +183,96 @@ def parse_config_file(config_function, sort_names):
     return comparison_options, datasets_list, datasets_labels_dict, datasets_titles_dict, algorithms_list, \
         algorithms_labels_dict, algorithms_file_names_dict, algorithms_tests_dict, algorithms_tests_labels_dict, \
         boxplot_distances_dict, boxplot_percentages_dict
+
+if __name__ == '__main__':
+
+    # Parsing Argument Options
+    parser = argparse.ArgumentParser(description='''Analyze trajectories''')
+
+    default_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                '../results')
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                               '../analyze_trajectories_config')
+
+    parser.add_argument('config', type=str,
+                        help='yaml file specifying algorithms and datasets')
+    parser.add_argument(
+        '--output_dir',
+        help="Folder to output plots and data",
+        default=default_path)
+    parser.add_argument(
+        '--results_dir', help='base folder with the results to analyze',
+        default=default_path)
+
+    parser.add_argument(
+        '--platform', help='HW platform: [laptop, nuc, odroid, up]',
+        default='laptop')
+    parser.add_argument(
+        '--mul_trials', type=int,
+        help='number of trials, None for single run', default=None)
+    parser.add_argument('--no_sort_names', action='store_false', dest='sort_names',
+                        help='whether to sort dataset and algorithm names')
+
+    # odometry error
+    parser.add_argument(
+        '--odometry_error_per_dataset',
+        help="Analyze odometry error for individual dataset. "
+             "The same subtrajectory length will be used for the same dataset "
+             "and different algorithms",
+        action='store_true')
+    parser.add_argument(
+        '--overall_odometry_error',
+        help="Collect the odometry error from all datasets and calculate statistics.",
+        dest='overall_odometry_error',
+        action='store_true')
+
+    # RMSE (ATE)
+    parser.add_argument(
+        '--rmse_table', help='Output rms erros into latex tables',
+        action='store_true')
+    parser.add_argument(
+        '--rmse_table_median_only', action='store_true', dest='rmse_median_only')
+    parser.add_argument('--rmse_boxplot',
+                        help='Plot the trajectories', action='store_true')
+
+    parser.add_argument('--write_time_statistics', help='write time statistics',
+                        action='store_true')
+
+    # plot trajectories
+    parser.add_argument('--plot_trajectories',
+                        help='Plot the trajectories', action='store_true')
+    parser.add_argument('--no_plot_side', action='store_false', dest='plot_side')
+    parser.add_argument('--no_plot_aligned', action='store_false', dest='plot_aligned')
+    parser.add_argument('--no_plot_traj_per_alg', action='store_false',
+                        dest='plot_traj_per_alg')
+
+    parser.add_argument('--recalculate_errors',
+                        help='Deletes cached errors', action='store_true')
+    parser.add_argument('--png',
+                        help='Save plots as png instead of pdf',
+                        action='store_true')
+    parser.add_argument('--dpi', type=int, default=300)
+    parser.set_defaults(odometry_error_per_dataset=False, overall_odometry_error=False,
+                        rmse_table=False,
+                        plot_trajectories=False, rmse_boxplot=False,
+                        recalculate_errors=False, png=False, time_statistics=False,
+                        sort_names=True, plot_side=True, plot_aligned=True,
+                        plot_traj_per_alg=True, rmse_median_only=False)
+
+    args = parser.parse_args()
+    print("Arguments:\n{}".format(
+        '\n'.join(['- {}: {}'.format(k, v)
+                   for k, v in args.__dict__.items()])))
+
+    print("Will analyze results from {0} and output will be "
+          "in {1}".format(args.results_dir, args.output_dir))
+    output_dir = args.output_dir
+
+    config_fn = os.path.join(config_path, args.config)
+
+    print("Parsing evaluation configuration {0}...".format(config_fn))
+
+    # Parsing the Configuration yaml file
+    comparison_opt, datasets, datasets_labels, datasets_titles, algorithms, algorithms_labels, \
+        algorithms_file_names, algorithms_tests, algorithms_tests_labels, boxplot_distances, \
+        boxplot_percentages = parse_config_file(config_fn, args.sort_names)
